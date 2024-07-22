36394a23163f023f6bc1c2979c433eda374fec0b
Ashutosh Shukla <ashutosh6471@gmail.com>
2024-07-22 16:42:00 +0530

    Updated Vehicle Routes and Server file

--- src/index.js
+++ src/index.js
@@ -3,7 +3,15 @@ const {serverConfig,Logger} = require('./config');
 const apiRoutes = require('./routes');
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use('/api',apiRoutes);
+app.use('/vehicleService/api',apiRoutes);
+
+
+
 app.listen(serverConfig.PORT,()=>{
     console.log(`Successfully started the server at port ${serverConfig.PORT}`);
     Logger.info('Successfully Started The Server','root',{});
--- src/routes/V1/index.js
+++ src/routes/V1/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const {InfoController} = require('../../controllers');
+const vehicleRoutes = require('./vehicle-routes');
 
 router.get('/info',InfoController.info);
+router.use('/vehicles',vehicleRoutes);
 module.exports = router;
